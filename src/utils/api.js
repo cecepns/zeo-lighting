7e409203fc58ac9bbe2dd34e6ee4beafccaf5df6
Cecep Nandang <cecep@kima.one>
2026-01-20 23:44:32 +0700

    fix: login

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://api-inventory.isavralabel.com/zero-lighting/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
